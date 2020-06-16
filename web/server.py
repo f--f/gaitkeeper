07b05faabe3df73fd6f12332751380a3e32639fa
Fred Fu <f--f@users.noreply.github.com>
2020-06-15 23:23:55 -0400

    Update frontend pages

--- web/server.py
+++ web/server.py
@@ -3,6 +3,7 @@ from psycopg2 import sql
 from psycopg2.extras import execute_values
 from create_db import connect_to_db
 import numpy as np
+import pandas as pd
 import json
 from datetime import datetime
 
@@ -53,26 +54,45 @@ def commit_recording_to_db(formdata):
                 [(walk_id, time[i], v[0], v[1], v[2]) for i, v in enumerate(values)])
     conn.commit()
     cur.close()
-    app.logger.info('Walk %d (%s) successfully committed to database!', walk_id, formdata["name"])
+    app.logger.info("Walk %d (%s) successfully committed to database!", walk_id, formdata["name"])
     return walk_id
 
 
-@app.route('/', methods=["GET"])
+@app.route("/", methods=["GET"])
 def home_page():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/upload', methods=["POST"])
+@app.route("/record", methods=["GET"])
+def record_page():
+    return render_template("record.html")
+
+
+@app.route("/compare", methods=["GET"])
+def compare_page(similarity=None):
+    # Get list of recorded walks
+    df_walks = pd.read_sql_query(f"SELECT name, date FROM walks ORDER BY date DESC", conn)
+    df_walks = df_walks[df_walks["name"] != ""]  # Ignore walks with no name
+    walk_names = df_walks["date"].astype(str) + " - " + df_walks["name"]
+    return render_template("compare.html", 
+        walk_names=walk_names,
+        similarity=similarity
+    )
+
+
+@app.route("/score", methods=["POST"])
+def calculate_similarity():
+    # Compute similarity score
+    return compare_page(similarity=0.9)
+
+
+@app.route("/upload", methods=["POST"])
 def upload_recording():
     # `request.form[sensor]` variable contains form data for each sensor type
-    # TODO: Check validity of data, is form data long enough / sensor frequency consistent
+    # TODO: Check validity of data in backend
+    # TODO: Output success message once uploaded
     user_id, walk_id = commit_recording_to_db(request.form)
-    # df = load.read_form_data(request.form)
-    # Placeholder for MVP...
-    df_html = """<table border="1" class="dataframe">\n  <thead>\n    <tr style="text-align: right;">\n      <th></th>\n      <th>Score</th>\n    </tr>\n    <tr>\n      <th>user_id</th>\n      <th></th>\n    </tr>\n  </thead>\n  <tbody>\n    <tr>\n      <th>1</th>\n      <td>0.717070</td>\n    </tr>\n    <tr>\n      <th>40</th>\n      <td>0.570926</td>\n    </tr>\n    <tr>\n      <th>38</th>\n      <td>0.566829</td>\n    </tr>\n    <tr>\n      <th>26</th>\n      <td>0.562242</td>\n    </tr>\n    <tr>\n      <th>10</th>\n      <td>0.553433</td>\n    </tr>\n    <tr>\n      <th>35</th>\n      <td>0.551320</td>\n    </tr>\n    <tr>\n      <th>4</th>\n      <td>0.540472</td>\n    </tr>\n    <tr>\n      <th>21</th>\n      <td>0.537381</td>\n    </tr>\n    <tr>\n      <th>7</th>\n      <td>0.533832</td>\n    </tr>\n    <tr>\n      <th>29</th>\n      <td>0.530744</td>\n    </tr>\n    <tr>\n      <th>36</th>\n      <td>0.530096</td>\n    </tr>\n    <tr>\n      <th>46</th>\n      <td>0.521490</td>\n    </tr>\n    <tr>\n      <th>2</th>\n      <td>0.515982</td>\n    </tr>\n    <tr>\n      <th>27</th>\n      <td>0.514777</td>\n    </tr>\n    <tr>\n      <th>31</th>\n      <td>0.514268</td>\n    </tr>\n    <tr>\n      <th>28</th>\n      <td>0.513650</td>\n    </tr>\n    <tr>\n      <th>23</th>\n      <td>0.511082</td>\n    </tr>\n    <tr>\n      <th>3</th>\n      <td>0.509721</td>\n    </tr>\n    <tr>\n      <th>24</th>\n      <td>0.509693</td>\n    </tr>\n    <tr>\n      <th>33</th>\n      <td>0.499536</td>\n    </tr>\n    <tr>\n      <th>34</th>\n      <td>0.483492</td>\n    </tr>\n    <tr>\n      <th>43</th>\n      <td>0.483233</td>\n    </tr>\n    <tr>\n      <th>20</th>\n      <td>0.481084</td>\n    </tr>\n    <tr>\n      <th>16</th>\n      <td>0.472545</td>\n    </tr>\n    <tr>\n      <th>25</th>\n      <td>0.455413</td>\n    </tr>\n    <tr>\n      <th>19</th>\n      <td>0.451412</td>\n    </tr>\n    <tr>\n      <th>18</th>\n      <td>0.439560</td>\n    </tr>\n    <tr>\n      <th>37</th>\n      <td>0.411404</td>\n    </tr>\n    <tr>\n      <th>30</th>\n      <td>0.401884</td>\n    </tr>\n  </tbody>\n</table>"""
-
-    # Return df.to_html and maybe a plot
-    return render_template('result.html', model_output=df_html) #df.to_html())
+    return compare_page()
 
 
 if __name__ == "__main__":
