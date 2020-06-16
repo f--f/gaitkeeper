d2a2a12c32a8734c9690dfd8ff3fd66ca6510933
Fred Fu <f--f@users.noreply.github.com>
2020-06-15 22:02:46 -0400

    Fix fillna bug and add more kwargs in resampling code
    df.fillna doesn't work if axis=1 (known pandas issue).

--- gaitkeeper/load.py
+++ gaitkeeper/load.py
@@ -2,25 +2,26 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import json
+from tqdm.auto import tqdm
 
 IDNET_PATH = Path(r"C:\Users\Fred\Documents\Insight\Gait Datasets\IDNet Database\IDNet_dataset")
 
-def _resample_merged_data(merged_df, hz=60, pad=1):
+def _resample_merged_data(merged_df, hz=60, pad=3, truncate_seconds=10):
     # Truncate each dataframe based on latest start / earliest end
     # Alternatively, truncate up to 10s from beginning and end to exclude for start/stop motions
-    latest_start_time = max(merged_df.notna().idxmax().max(), merged_df.index[0] + pd.Timedelta("10s"))
-    earliest_end_time = min(merged_df.notna()[::-1].idxmax().min(), merged_df.index[-1] - pd.Timedelta("10s"))
+    latest_start_time = max(merged_df.notna().idxmax().max(), merged_df.index[0] + pd.Timedelta(f"{truncate_seconds}s"))
+    earliest_end_time = min(merged_df.notna()[::-1].idxmax().min(), merged_df.index[-1] - pd.Timedelta(f"{truncate_seconds}s"))
     # Sensors have different sampling freqs so resample
     merged_df = merged_df.resample(f"{round(1/hz * 1e9)}N").mean()
     merged_df = merged_df.loc[latest_start_time: earliest_end_time].copy()
     # Impute NaN values which are just resulting from a single skip
-    merged_df = merged_df.fillna(method='pad', limit=pad, axis=1)
+    merged_df = merged_df.fillna(method='pad', limit=pad)
     # Convert timestamp to relative times
     merged_df = merged_df.set_index(merged_df.index - merged_df.index[0])
     return merged_df
 
 
-def get_reference_data(user_id, walk_num, hz=60):
+def get_reference_data(user_id, walk_num, hz=60, pad=3):
     """Return DataFrame of walk sensor data from IDNet individual/walk at 10ms intervals (resampled).
     Note the data may include NaN values since sensors are sampled at different frequencies or may skip measurements."""
     # Retrieve folder
@@ -39,7 +40,7 @@ def get_reference_data(user_id, walk_num, hz=60):
         df = df.set_index("timestamp")
         dfs.append(df)
     merged_df = pd.concat(dfs, axis="columns")
-    return _resample_merged_data(merged_df, hz=hz)
+    return _resample_merged_data(merged_df, hz=hz, pad=pad)
 
 
 def read_form_data(formdata, hz=60):
